a28769932344fc29f0db8dac717a37ce5a674cc0
Tom Papas <tompapas@toms-air.gateway>
2019-04-19 09:04:39 +1000

    created a file with the functions

---
+++ MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/functions.py
@@ -0,0 +1,68 @@
+
+def is_word_guessed(secret_word, letters_guessed):
+    '''
+    secret_word: string, the word the user is guessing; assumes all letters are
+      lowercase
+    letters_guessed: list (of letters), which letters have been guessed so far;
+      assumes that all letters are lowercase
+    returns: boolean, True if all the letters of secret_word are in letters_guessed;
+      False otherwise
+    '''
+    my_String = True   
+    for i in secret_word:
+        if i not in letters_guessed:
+            my_String = False
+    return(my_String)
+
+
+def get_guessed_word(secret_word, letters_guessed):
+    '''
+    secret_word: string, the word the user is guessing
+    letters_guessed: list (of letters), which letters have been guessed so far
+    returns: string, comprised of letters, underscores (_), and spaces that represents
+      which letters in secret_word have been guessed so far.
+    '''
+    my_String = ''
+    for x in secret_word:
+         if x in letters_guessed:
+              my_String += x
+         else:
+              my_String += '_ '
+    return my_String
+
+
+
+def get_available_letters(letters_guessed):
+    '''
+    letters_guessed: list (of letters), which letters have been guessed so far
+    returns: string (of letters), comprised of letters that represents which letters have not
+      yet been guessed.
+    '''
+    lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    my_String = ''
+    for x in lowercase:
+       if x not in letters_guessed:
+          my_String += x
+    return my_String
+
+#letters_guessed = ['e', 'i', 'k', 'p', 'r', 's']
+#print (get_available_letters(letters_guessed))
+
+def updates(a):
+      number_guesses=a[0]
+      guess=a[1]
+      number_warnings=a[2]
+      c=guess.isalpha() 
+       #######################
+       #updates        
+       letters_guessed = letters_guessed + guess   
+       word_guess = get_guessed_word(secret_word, letters_guessed)   
+       #Display to user whether guess in secret word       
+       if guess in secret_word:
+           print("Good guess", word_guess)
+       elif guess in ("a","e","i","o","u"):
+           print("Bad guess", word_guess)
+           number_guesses = number_guesses -2
+       else:   
+           print("Bad guess", word_guess)
+           number_guesses = number_guesses -1
--- MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
+++ MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
@@ -2,13 +2,27 @@ import unittest
 import sys
 sys.path.append("/Users/tompapas/Documents/Programming/Programming-Training/MIT6_0001F16/Assignments/Problem Set 2")
 import print_messages
+import functions
 
 class test_print_messages(unittest.TestCase):
   """docstring for ClassName"""
   def test_print_messages(self):
     self.assertEqual(print_messages.print_messages(2,2,"a"), None)
     self.assertEqual(print_messages.print_messages(2,2,""), None)
-  3
+  def test_iswordguessed(self):
+    self.assertEqual(functions.is_word_guessed("marina",[]), False)
+    self.assertEqual(functions.is_word_guessed("marina",["a","b","c"]), False)
+    self.assertEqual(functions.is_word_guessed("marina",["a","r","i","m", "n"]), True)
+    self.assertEqual(functions.is_word_guessed("marina",["a","r","i","m", "N"]), False)
+  def test_getguessword(self):
+    self.assertEqual(functions.get_guessed_word("marina",[]), "_ _ _ _ _ _ ")
+    self.assertEqual(functions.get_guessed_word("marina",["m","a"]), "ma_ _ _ a")
+    self.assertEqual(functions.get_guessed_word("marina",["m","a","a"]), "ma_ _ _ a")
+    self.assertEqual(functions.get_guessed_word("marina",["m","a","a","r","n","i"]), "marina")
+  def test_getavailableletters(self):
+    self.assertEqual(functions.get_available_letters(['m','a','r','i','n','a']), 'bcdefghjklopqstuvwxyz')
+    self.assertEqual(functions.get_available_letters(['m','a','r','i','2','1']), 'bcdefghjklnopqstuvwxyz')
+    self.assertEqual(functions.get_available_letters([]), 'abcdefghijklmnopqrstuvwxyz')
 if __name__ == '__main__':
   unittest.main()
     
