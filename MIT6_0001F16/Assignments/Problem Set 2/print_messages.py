bd280228daefd33fb3c524af104713f29a3e9a1b
Tom Papas <tompapas@toms-air.gateway>
2019-04-25 09:51:44 +1000

    removed number of warnings from print messages function

--- MIT6_0001F16/Assignments/Problem Set 2/print_messages.py
+++ MIT6_0001F16/Assignments/Problem Set 2/print_messages.py
@@ -1,12 +1,10 @@
 import get_available_letters
 
-def print_messages(number_warnings,number_guesses,letters_guessed):
+def print_messages(number_guesses,letters_guessed):
     '''
     number of warnings, number of guesses and letters guessed so far.
-    returns: prints how many warings, how many guesses and the letters available to select from.
+    returns: prints how many guesses and the letters available to select from.
     '''
-    print("You have",number_warnings,"warnings remaining") 
-    #after each guess need update warnings, guesses, letters guessed and word guess
     print("-----------------")        
     print("You have",number_guesses,"guesses remaining")
     #show the available letters to user      
