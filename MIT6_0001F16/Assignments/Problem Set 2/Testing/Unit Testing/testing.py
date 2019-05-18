4b40e465e7bb060114d091995fe665b2a679f449
Tom Papas <tompapas@toms-air.gateway>
2019-05-19 09:15:17 +1000

    removed functions file

--- MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
+++ MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
@@ -1,15 +1,14 @@
 import unittest
 import sys
 sys.path.append("/Users/tompapas/Documents/Programming/Programming-Training/MIT6_0001F16/Assignments/Problem Set 2")
-import print_messages
 import functions
-import valid_guess
+
 
 class test_print_messages(unittest.TestCase):
   """docstring for ClassName"""
   def test_print_messages(self):
-    self.assertEqual(print_messages.print_messages(2,"a"), None)
-    self.assertEqual(print_messages.print_messages(2,""), None)
+    self.assertEqual(functions.print_messages(2,"a"), None)
+    self.assertEqual(functions.print_messages(2,""), None)
   def test_iswordguessed(self):
     self.assertEqual(functions.is_word_guessed("marina",[]), False)
     self.assertEqual(functions.is_word_guessed("marina",["a","b","c"]), False)
@@ -25,9 +24,9 @@ class test_print_messages(unittest.TestCase):
     self.assertEqual(functions.get_available_letters(['m','a','r','i','2','1']), 'bcdefghjklnopqstuvwxyz')
     self.assertEqual(functions.get_available_letters([]), 'abcdefghijklmnopqrstuvwxyz')
   def test_update(self): 
-    self.assertEqual(functions.updates(2, "a", ['n'], "marina"), (2,["n","a"]))
-    self.assertEqual(functions.updates(2, "c", ['b'], "marina"), (1,["b","c"]))
-    self.assertEqual(functions.updates(2, "q", ['b','f'], "marina"), (1,["b","f","q"]))
+    self.assertEqual(functions.updates(2, "a", ['n'], "marina"), (2,["n","a"],'_ a_ _ na'))
+    self.assertEqual(functions.updates(2, "c", ['b'], "marina"), (1,["b","c"], '_ _ _ _ _ _ '))
+    self.assertEqual(functions.updates(2, "q", ['b','f'], "marina"), (1,["b","f","q"], '_ _ _ _ _ _ '))
   #def test_validguess(self):
     #valid guess - user enters "f"
     #self.assertEqual(valid_guess.valid_guess('abc', '_ _ _ _ ',3,2), (3, "f", 2)) 
