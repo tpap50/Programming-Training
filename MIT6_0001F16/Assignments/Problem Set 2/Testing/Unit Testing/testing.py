8be8ce233d187515607231bb3580ba7d6655d035
Tom Papas <tompapas@toms-air.gateway>
2019-06-29 19:38:36 +1000

    testing

--- MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
+++ MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append("/Users/tompapas/Documents/Programming/Programming-Training/MIT6_0001F16/Assignments/Problem Set 2")
 import functions
 class test_update(unittest.TestCase):
+  
   def test_update(self):
       self.assertEqual(functions.updates(2, "a", ['n'], "marina"), (2,["n","a"],'_ a_ _ na'))
       self.assertEqual(functions.updates(2, "c", ['b'], "marina"), (1,["b","c"], '_ _ _ _ _ _ '))
@@ -16,7 +17,7 @@ class test_update(unittest.TestCase):
   def test_validguess(self):
       with patch('builtins.input', return_value="f"):
         self.assertEqual(functions.valid_guess('abc', '_ _ _ _ ',3,2), (3, "f", 2))  
-      with patch('builtins.input', side_effects=("a","f")):
+      with patch('builtins.input', side_effect=("a","f")):
         self.assertEqual(functions.valid_guess('abc','_ _ _ _',3,2), (3,"f",1))
     #self.assertEqual(valid_guess.valid_guess('abc', '_ _ _ _ ',3,2), (3, "f", 1))
     #letter already guess and there are no warnings, user enters "a", followed by "f", lose a guess.
