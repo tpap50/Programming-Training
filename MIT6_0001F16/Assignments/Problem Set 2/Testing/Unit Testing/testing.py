564550d92d8de56651eb9a6ee879215dbb372573
Tom Papas <tompapas@toms-air.gateway>
2019-07-02 19:03:16 +1000

    you beauty

--- MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
+++ MIT6_0001F16/Assignments/Problem Set 2/Testing/Unit Testing/testing.py
@@ -37,10 +37,15 @@ class test_update(unittest.TestCase):
     #guess a letter already guessed followed by one not guessed so far - reduce the number of warnings to zero and lose all guesss   
     with patch('builtins.input', side_effect=("a","b","c","b","c","f")):
         self.assertEqual(functions.valid_guess('abc','_ _ _ _',3,2), (0,"f",0))
-     #guess a letter already guessed followed by one not guessed so far - reduce the number of warnings to zero and lose all guesss   
-    with patch('builtins.input', side_effect=("a","b","1","f")):
-        self.assertEqual(functions.valid_guess('abc','_ _ _ _',3,2), (2,"f",0))
+     #guess a letter already guessed followed by two  numbers and finally a valid guessir - reduce the number of warnings to zero   
+    with patch('builtins.input', side_effect=("a","b","1","2","f")):
+        self.assertEqual(functions.valid_guess('abc','_ _ _ _',3,2), (1,"f",0))
     #self.assertEqual(valid_guess.valid_guess('abc', '_ _ _ _ ',3,2), (3, "f", 1))
     #letter already guess and there are no warnings, user enters "a", followed by "f", lose a guess.
+  def test_hangman(self):
+  #guess a letter not guessed so far  
+    with patch('builtins.input', side_effect=("t","o","m")):
+        self.assertEqual(functions.hangman("tom"), (["","t","o","m"],6,True))  
+ 
 if __name__ == '__main__':
   unittest.main()
