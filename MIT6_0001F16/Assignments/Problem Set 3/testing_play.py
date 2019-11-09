a6587b0fd4f89a8fece830aaea45f030150c1de8
Tom Papas <tompapas@toms-air.modem>
2019-11-09 16:04:52 +1100

    fixed get word score

--- MIT6_0001F16/Assignments/Problem Set 3/testing_play.py
+++ MIT6_0001F16/Assignments/Problem Set 3/testing_play.py
@@ -2,7 +2,7 @@
 import unittest
 import sys
 sys.path.append("/Users/tompapas/Documents/Programming/Programming-Training/MIT6_0001F16/Assignments/Problem Set 3")
-import functions_play
+import ps3test
 
 words = {("", 7):0, ("it", 7):2, ("was", 7):54, ("weed", 6):176,
              ("scored", 7):351, ("WaYbILl", 7):735, ("Outgnaw", 7):539,
@@ -13,10 +13,10 @@ words = {("", 7):0, ("it", 7):2, ("was", 7):54, ("weed", 6):176,
 class test_update(unittest.TestCase):
   def testget_word_score(self):
     for (word, n) in words.keys():
-    	score=words[(word,n)]
-        print((word,n))
-        print(score)
-        self.assertEqual(functions_play.get_word_score(word,n), score)
+      score=words[(word,n)]
+     #   print((word,n))
+     #   print(score)
+      self.assertEqual(ps3test.get_word_score(word,n), score)
 
 if __name__ == '__main__':
   unittest.main()
