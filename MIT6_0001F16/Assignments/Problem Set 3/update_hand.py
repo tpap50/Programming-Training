111f5d93bc1a6f46b6735596e090d3178b589559
Tom Papas <tompapas@toms-air.modem>
2019-11-16 05:58:32 +1100

    started the function update_hand

---
+++ MIT6_0001F16/Assignments/Problem Set 3/update_hand.py
@@ -0,0 +1,9 @@
+# step 1 we need the hand argument to be a string
+
+def update_handi(hand, word):
+  for i in word:
+   #lookup in i in hand and if exists we substract 1 from value so long as value is greater than zeor
+#1. access the ith element from word
+#2  lookup in the hand dictionary use the 'get' function
+#3 update hand
+   
